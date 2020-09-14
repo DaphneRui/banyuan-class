7dd2382711a54df8863dee2d16974831746e1add
ruixiang <1900956090@qq.com>
2020-09-14 10:36:36 +0800

    911homework

--- 911hw/src/App.vue
+++ 911hw/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    
+    <Work></Work>
   </div>
 </template>
 
 <script>
-
+import Work from './components/work'
 
 export default {
   name: "App",
   components: {
-    
+    Work
   }
 };
 </script>
