6be5395e839ea625eaa7ae4e85404635af5b1798
ruixiang <1900956090@qq.com>
2020-09-16 09:01:24 +0800

    915homework

---
+++ 915hw/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="whole">
+    <Aliyun></Aliyun>
+  </div>
+</template>
+
+<script>
+import Aliyun from "./components/aliyun"
+
+export default {
+  name: "App",
+  components: {
+    Aliyun
+  }
+};
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+.whole{
+  width: 1440px;
+  margin: 0 auto;
+}
+</style>
---
+++ 915hw/src/components/aliyun.vue
@@ -0,0 +1,359 @@
+<template>
+  <div>
+    <div class="topbar">
+        <div class="topbar-header">
+            <div class="aliyun-logo">
+                <img src="https://img.alicdn.com/tfs/TB1Ly5oS3HqK1RjSZFPXXcwapXa-238-54.png" alt="">
+            </div>
+            <div class="aliyun-language">
+                <span>中国站</span>
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </div>
+            <div class="aliyun-search">
+                <input type="text" name="" id="" placeholder="企业用户专属优惠">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <button>搜索</button>
+                <div class="search-tab" style="left: -2px; background: #ff6a00; color: #fff;">全部</div>
+                <div class="search-tab" style="left: 45px;">域名</div>
+                <div class="search-tab" style="left: 92px;">商标</div>
+                <div class="search-tab" style="left: 139px;">公司</div>
+                <div class="search-hotkey">
+                    <a href="" style="padding-left: 0px;">商标智能注册</a>
+                    <a href="">企业必备产品榜单</a>
+                    <a href="">云服务器 ECS</a>
+                    <a href="">弹性公网IP</a>
+                    <a href="" style="border-right: none;">SSL证书</a>
+                </div>
+            </div>
+            <div class="aliyun-ability">
+                <a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i>购物车</a>
+                <a href="">文档</a>
+                <a href="">备案</a>
+                <a href="">控制台</a>
+                <a href="">登录</a>
+                <button>免费注册</button>
+            </div>
+        </div>
+
+        <div class="topbar-menu">
+            <a href="" style="padding-left:0px; padding-right:45px">最新活动
+                <img src="https://img.alicdn.com/tfs/TB1q47VbUD1gK0jSZFGXXbd3FXa-66-24.png" alt="">
+            </a>
+            <a href="">产品分类</a>
+            <a href="">企业应用中心</a>
+            <a href="">解决方案</a>
+            <a href="">云市场</a>
+            <a href="">支持与服务</a>
+            <a href="">合作伙伴与生态</a>
+            <a href="">开发者</a>
+            <a href="">云栖号</a>
+            <a href="">了解阿里云</a>
+            <img src="https://gw.alicdn.com/tfs/TB1QKiexhD1gK0jSZFKXXcJrVXa-720-200.gif" alt="">
+            
+        </div>
+    </div>
+
+    <div class="floor1">
+        <div class="slot">
+            <div class="slot-left">
+                <span>精选爆款</span>
+                <div class="slot-list">
+                    <div class="slot-list-item">
+                        <a href="">弹性计算
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">云服务器 ECS</a>
+                            <a href="" style="border: 0">轻量应用服务器</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">数据库
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">MySQL</a>
+                            <a href="">SQL Server</a>
+                            <a href="" style="border: 0">Redis</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">域名与网站
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">域名注册</a>
+                            <a href="">网站建设</a>
+                            <a href="" style="border: 0">云虚拟主机</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">存储与网络
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">对象存储OSS</a>
+                            <a href="">负载均衡</a>
+                            <a href="" style="border: 0">CDN</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">云安全
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">SSL证书</a>
+                            <a href="">云安全中心</a>
+                            <a href="" style="border: 0">WAF</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">物联网与云通信
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">企业物联网</a>
+                            <a href="">边缘计算</a>
+                            <a href="" style="border: 0">短信服务</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">大数据与人工智能
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key">
+                            <a href="">MaxCompute</a>
+                            <a href="" style="border: 0">智能语音交互</a>
+                        </div>
+                    </div>
+                    <div class="slot-list-item">
+                        <a href="">企业应用服务
+                            <i class="fa fa-angle-right" aria-hidden="true"></i>
+                        </a>
+                        <div class="slot-item-key"  style="border-bottom: none">
+                            <a href="">工商财税</a>
+                            <a href="">商标注册</a>
+                            <a href="" style="border: 0">企业邮箱</a>
+                        </div>
+                    </div>                   
+                </div>
+                <div class="slot-bottom">查看全部</div>
+            </div>
+            <div class="slot-right">
+                <img src="https://img.alicdn.com/tfs/TB14qePjk9l0K4jSZFKXXXFjpXa-1880-640.jpg" alt="">
+                <div class="slot-right-bottom">
+                    <div class="slot-right-box">
+                        <div class="slot-right-box-item">
+                            <div class="box-item-title"><a>会员权益</a></div>
+                            <div class="box-item-desc">新版上线，会员积分超值兑，海量优惠待你领</div>
+                            <img src="https://img.alicdn.com/tfs/TB11iLqVMHqK1RjSZFEXXcGMXXa-128-128.png" alt="">
+                        </div>
+                        <div class="slot-right-box-item">
+                            <div class="box-item-title"><a>爆款特惠</a></div>
+                            <div class="box-item-desc">精选爆款产品低至0.95折，云服务器ECS 8.1元/月</div>
+                            <img src="https://img.alicdn.com/tfs/TB15U_0ArH1gK0jSZFwXXc7aXXa-128-128.png" alt="">
+                        </div>
+                        <div class="slot-right-box-item" style="border: 0px">
+                            <div class="box-item-title"><a>新品发布</a></div>
+                            <div class="box-item-desc">大数据年度十二大发布，新品折扣、爆款特惠19.9元</div>
+                            <img src="https://img.alicdn.com/tfs/TB1u0URc2c3T1VjSZLeXXbZsVXa-128-128.png" alt="">
+                        </div>
+                    </div>
+                    <div class="slot-right-user">
+                        <img src="https://img.alicdn.com/tfs/TB1Sv5PV9zqK1RjSZPcXXbTepXa-80-80.png" alt="">
+                        <p>Hi!欢迎来到阿里云</p>
+                        <div class="user-button">
+                            <button>登录</button>
+                            <button>注册</button>
+                        </div>
+                        <div class="user-link">
+                            <a href="">会员云计算</a>
+                            <span> | </span>
+                            <a href="">免费试用</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="floor2">
+        <div class="title">企业应用中心</div>
+        <div class="solve">
+            <div class="solve-left">
+                <div class="solve-left-title">
+                    <p>买软件、选服务</p>
+                    <p>就上阿里云应用中心</p>
+                </div>
+                <div class="solve-left-title-des">
+                    <p>提供工商财税、知识产权、域名等多项企业刚需服务</p>
+                </div>
+                <a href="">立即访问</a>
+            </div>
+            <div class="solve-right">
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">专家专人服务</span>
+                        <span>管理维护透明</span>
+                        <span>智能高效</span>
+                    </div>
+                    <div class="solve-card-title">网络文化经营许可证</div>
+                    <div class="solve-card-des">
+                        助力企业资质安全<br>
+                        专家专人服务，进度实时可查<br>
+                        从事互联网文化活动必备资质
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 1600</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">新品发布</span>
+                        <span>10秒一键生成</span>
+                    </div>
+                    <div class="solve-card-title">智能LOGO设计</div>
+                    <div class="solve-card-des">
+                        AI算法，10秒定制专属logo<br>
+                        海量创意，支持免费在线制作<br>
+                        智能精准匹配，解决多场景应用
+                    </div>
+                    <div class="solve-card-money">
+                        <span style="font-size: 14px;">免费在线体验</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">急速申报</span>
+                        <span>进度透明</span>
+                    </div>
+                    <div class="solve-card-title">商标注册申请</div>
+                    <div class="solve-card-des">
+                        审核后最快1分钟递交商标局<br>
+                        支持一键加入阿里电商品牌库<br>
+                        流程透明可控，随时查看进度
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 280</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">无需线下跑腿</span>
+                        <span>首购领券享优惠</span>
+                    </div>
+                    <div class="solve-card-title">软件著作权登记</div>
+                    <div class="solve-card-des">
+                        无纸化极速办理，节省时间<br>
+                        全流程可视化，进度实时可查<br>
+                        系统自动纠错，最快3个工作日拿证
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 300</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">低至9.9元</span>
+                        <span>最快1天发照</span>
+                    </div>
+                    <div class="solve-card-title">云上公司/个体工商户注册</div>
+                    <div class="solve-card-des">
+                        政府提供注册地址，支持远程异地申请<br> 
+                        享园区优惠政策<br> 
+                        在线办理，最快1天发照                       
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 9.9</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">创业第一步</span>
+                        <span>最快1天递交</span>
+                    </div>
+                    <div class="solve-card-title">公司注册</div>
+                    <div class="solve-card-des">
+                        注册城市全面覆盖，助力创业第一步<br>
+                        企业名称免费查询，一对一免费咨询<br>
+                        最快1天递交系统，申请进度实时掌控
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 68</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">线上随时查账</span>
+                        <span>出错全额赔付</span>
+                    </div>
+                    <div class="solve-card-title">代理记账</div>
+                    <div class="solve-card-des">
+                        资深团队多对一服务保障<br>
+                        记账出错全额赔付<br>
+                        一站式专业服务
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 1200</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">模板建站</span>
+                        <span>含服务器</span>
+                        <span>即开即用</span>
+                    </div>
+                    <div class="solve-card-title">云·速成美站</div>
+                    <div class="solve-card-des">
+                        1153套PC+手机网站模板，免费随意切换<br>
+                        可视化后台类PPT操作，自己动手建网站
+                    </div>
+                    <div class="solve-card-money">
+                        <span>￥ 500</span>
+                        <span> 起</span>
+                    </div>
+                </div>
+                <div class="solve-body-card">
+                    <div class="solve-card-tab">
+                        <span style="margin-left:0px">建站爆款</span>
+                        <span>心选品质</span>
+                        <span>含服务器</span>
+                    </div>
+                    <div class="solve-card-title">云·企业官网</div>
+                    <div class="solve-card-des">
+                        1对1网站策划及定制设计，不满意全额退款<br>
+                        PC站+手机站+互动表单+会员支付多应用场景
+                    </div>
+                    <div class="solve-card-money">
+                        <span style="font-size: 14px;color: #ff6a00;">咨询立减500元起</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "aliyun",
+  components: {
+    
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../style/aliyun"    
+</style>>
+
+
